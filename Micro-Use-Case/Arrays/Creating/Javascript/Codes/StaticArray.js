645405ee650c881eb359c3fd118a087006fda795
Shobhit Sharma <shobhit0812@gmail.com>
2023-08-01 18:56:55 +0530

    clean

---
+++ Micro-Use-Case/Arrays/Creating/Javascript/Codes/StaticArray.js
@@ -0,0 +1,2 @@
+arr1 = new Array(4);
+arr2 = new Array("Gaurav", 28, "Frontend");
---
+++ Micro-Use-Case/Arrays/Operations/Java/Codes/ArrayRotate.java
@@ -0,0 +1,49 @@
+public class ArrayRotate {
+    
+    public static void main(String[] args) {
+        
+        int arr[] = new int[] { 1, 2, 3, 4};
+
+        int maxE = 0;
+        int maxI = -1;
+
+        for(int i = 1; i <= arr.length - 1; i++) {
+            if(arr[i - 1] > arr[i]) {
+                maxE = arr[i - 1];
+                maxI = i - 1;
+            }
+
+            else {
+                maxE = arr[i];
+                maxI = i;
+            }
+        }
+
+        rotate(arr, maxI);
+
+        System.out.println("maxE: " + maxE);
+
+        for(int elements: arr) {
+            System.out.print(elements + " ");
+        }
+
+    }
+
+
+
+    public static void rotate(int arr[], int maxI) {
+
+        if(maxI == 0) {
+            return;
+        }
+
+        else {
+            for(int i = maxI; i > 0; i--) {
+                int temp = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temp;
+            }
+        }
+        
+    }
+}
