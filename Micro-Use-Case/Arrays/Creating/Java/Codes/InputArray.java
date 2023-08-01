f818f78f00ad0c85d9d1667ad46b1813e9e03c70
Shobhit Sharma <shobhit0812@gmail.com>
2023-08-01 12:51:39 +0530

    Create InputArray.java

---
+++ Micro-Use-Case/Arrays/Creating/Java/Codes/InputArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class InputArray {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        // Taking array length size
+
+        int N = sc.nextInt();
+
+        // Creating integer array of N size
+
+        int arrayInt[] = new int[N];
+
+        // traversing ith element of array and updaing input value to each position
+        for(int i = 0; i < N; i++) {
+            arrayInt[i] = sc.nextInt();
+        }
+
+        for(int element : arrayInt) {
+            System.out.println("Array Element: " + element);
+        }
+    }
+}
