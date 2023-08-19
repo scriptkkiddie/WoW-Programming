0910d62eb3a91a822a8acb95a212590c126b3ac6
Shobhit Sharma <shobhit0812@gmail.com>
2023-08-19 13:57:55 +0530

    clean

---
+++ Micro_Use_Case/Arrays/Operations/Sum/Java/Codes/SumFormula.java
@@ -0,0 +1,18 @@
+package Micro_Use_Case.Arrays.Operations.Sum.Java.Codes;
+
+public class SumFormula {
+
+    public static void main(String[] args) {
+
+        // Sum of Linear Elements
+        
+        int[] arr = {1, 2, 3, 4, 5};
+        int N = 5;
+
+        // for(int i = 0; i < N; i++) {
+            
+        // }
+
+        System.out.println((N * (N + 1)) / 2);
+    }
+}
